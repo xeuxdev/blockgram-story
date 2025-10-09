d77bec86874bd5512c1c38dade75e7c99c9acafb
xeuxdev <tochukwujohn24@gmail.com>
2025-10-09 13:50:54 +0100

    update to fit netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
 });
